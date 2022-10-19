d1771dc776216154f9d0868244d7964673419c91
Christopher Mwangi <23134282@airtel.africa>
2022-10-19 22:26:23 +0300

    added send api

--- src/main/java/com/airtel/controller/ApplicationController.java
+++ src/main/java/com/airtel/controller/ApplicationController.java
@@ -2,6 +2,8 @@ package com.airtel.controller;
 
 import com.airtel.model.message_alerts.CustomerMessage;
 import com.airtel.service.IWhatsappService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class ApplicationController {
 
+    Logger logger = LoggerFactory.getLogger(ApplicationController.class);
     @Autowired
     private IWhatsappService  whatsappService;
 
@@ -25,12 +28,22 @@ public class ApplicationController {
         String token = request.getParameter("hub.verify_token");
         String challenge = request.getParameter("hub.challenge");
 
+        logger.info("mode : {} : token : {} : challenge : {} ",mode,token,challenge);
+
         return whatsappService.verifyToken(mode,token,challenge);
     }
 
     @RequestMapping(path = "/webhook", method = RequestMethod.POST)
     public void message(@RequestBody CustomerMessage message){
         whatsappService.messageAlert(message);
+    }
+
+    @RequestMapping(path = "/send", method = RequestMethod.GET)
+    public void sendText(HttpServletRequest request){
+        String msisdn = request.getParameter("msisdn");
+        logger.info("msisdn : {}",msisdn);
+        whatsappService.sendMessage(msisdn);
+
     }
 
 
